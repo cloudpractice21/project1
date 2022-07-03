5d3b20f7538291cbf3b45137d9b2acd419ef8b4e
sudhakaralavala <sudhakarinfomail21@gmail.com>
2022-07-03 11:08:45 +0200

    This is a new commit for what I originally planned to be amended

---
+++ hello.py
@@ -0,0 +1,14 @@
+class Mobiles:
+    def __init__(self,brand,mobile,price):
+        self.brand=brand
+        self.mobile=mobile
+        self.price=price
+    def disp(self):
+        print("Brand:",self.brand)
+        print("Mobile:",self.mobile)
+        print("Price:",self.price)
+        print("...................")
+
+for i in range(5): 
+    obj=Mobiles("Apple","I10","20000")
+    obj.disp()
